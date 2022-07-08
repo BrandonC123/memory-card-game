abf11c82e0a19d7cff73046dd8de2fab87d72b5e
BrandonC123 <brandoc318@gmail.com>
2022-07-08 15:10:46 -0700

    Add in rough styling, created header component, and updated scoreboard with instructions. Need to implement keeping track of scores next.

---
+++ memory-card-game/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = (props) => {
+    return (
+        <header className="header">
+            <div className="header-container container row">
+                <img src="./img/header-img.svg" alt="Header card icon" />
+                <h2 className="header-title">Memory Game</h2>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- memory-card-game/src/components/Scoreboard.jsx
+++ memory-card-game/src/components/Scoreboard.jsx
@@ -1,3 +1,18 @@
 const Scoreboard = () => {
-    return <div></div>;
+    return (
+        <div className="scoreboard-container container row">
+            <div className="instruction-container">
+                <p>Instructions:</p>
+                <p>Below are 4 different card options</p>
+                <p>Choose as many unique as possible to increase your points</p>
+                <p>Points can be see on the right</p>
+            </div>
+            <div className="score-content-container">
+                <p>Score: 1</p>
+                <p>Best Score: 1</p>
+            </div>
+        </div>
+    );
 };
+
+export default Scoreboard;
