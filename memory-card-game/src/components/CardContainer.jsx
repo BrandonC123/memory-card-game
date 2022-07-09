8fe1fbfed91778dcbf683207d8a8ba98bb683c10
BrandonC123 <brandoc318@gmail.com>
2022-07-08 20:58:43 -0700

    Refactor imgArray and reset selectedPics once a duplicate is chosen to have multiple rounds.

--- memory-card-game/src/components/CardContainer.jsx
+++ memory-card-game/src/components/CardContainer.jsx
@@ -5,16 +5,18 @@ import Card from "./Card";
 
 const CardContainer = (props) => {
     const [imgArray, setImgArray] = useState([
-        { imgTitle: "CPU Cooler", imgUrl: "./img/cpu-cooler-pic.jpg", id: 0 },
-        { imgTitle: "CPU", imgUrl: "./img/cpu-pic.jpg", id: 0 },
-        { imgTitle: "Graphics Card", imgUrl: "./img/gpu-pic.jpg", id: 1 },
-        { imgTitle: "Hard Drive", imgUrl: "./img/hard-drive-pic.jpg", id: 2 },
-        { imgTitle: "Headphone", imgUrl: "./img/headphone-pic.jpg", id: 0 },
-        { imgTitle: "Keyboard", imgUrl: "./img/keyboard-pic.jpg", id: 3 },
-        { imgTitle: "Monitor", imgUrl: "./img/monitor-pic.jpg", id: 4 },
-        { imgTitle: "Motherboard", imgUrl: "./img/motherboard-pic.jpg", id: 5 },
-        { imgTitle: "Mouse", imgUrl: "./img/mouse-pic.jpg", id: 6 },
-        { imgTitle: "Solid State Drive", imgUrl: "./img/ssd-pic.jpg", id: 7 },
+        { imgTitle: "Computer", imgUrl: "./img/computer-pic.jpg", id: 0 },
+        { imgTitle: "CPU Cooler", imgUrl: "./img/cpu-cooler-pic.jpg", id: 1 },
+        { imgTitle: "CPU", imgUrl: "./img/cpu-pic.jpg", id: 2 },
+        { imgTitle: "Graphics Card", imgUrl: "./img/gpu-pic.jpg", id: 3 },
+        { imgTitle: "Hard Drive", imgUrl: "./img/hard-drive-pic.jpg", id: 4 },
+        { imgTitle: "Headphone", imgUrl: "./img/headphone-pic.jpg", id: 5 },
+        { imgTitle: "Keyboard", imgUrl: "./img/keyboard-pic.jpg", id: 6 },
+        { imgTitle: "Monitor", imgUrl: "./img/monitor-pic.jpg", id: 7 },
+        { imgTitle: "Motherboard", imgUrl: "./img/motherboard-pic.jpg", id: 8 },
+        { imgTitle: "Mouse", imgUrl: "./img/mouse-pic.jpg", id: 9 },
+        { imgTitle: "Speaker", imgUrl: "./img/speaker-pic.jpg", id: 10 },
+        { imgTitle: "Solid State Drive", imgUrl: "./img/ssd-pic.jpg", id: 11 },
     ]);
     const [randomNum, setRandomNum] = useState([]);
     const [selectedPics, setSelectedPics] = useState([]);
@@ -36,6 +38,7 @@ const CardContainer = (props) => {
             console.log(selectedPics);
             console.log(selection);
             props.addCurrentScore(false);
+            setSelectedPics([]);
         } else {
             setSelectedPics([...selectedPics, selection]);
             props.addCurrentScore(true);
